99ea9b1d86d424598526712e5be84fc64d43da06
Kin Lo <kinabcd@gmail.com>
2018-08-16 19:14:52 +0800

    Give the new bubble a free space

--- src/main/java/tw/lospot/kin/call/bubble/BubbleList.kt
+++ src/main/java/tw/lospot/kin/call/bubble/BubbleList.kt
@@ -1,8 +1,10 @@
 package tw.lospot.kin.call.bubble
 
 import android.content.Context
+import android.graphics.Point
 import android.provider.Settings
 import android.util.TypedValue
+import android.view.WindowManager
 import tw.lospot.kin.call.connection.Call
 import tw.lospot.kin.call.connection.CallList
 import java.util.*
@@ -12,6 +14,9 @@ import java.util.*
  */
 
 class BubbleList(val context: Context) : CallList.Listener {
+    private val px64dp = TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 64f, context.resources.displayMetrics).toInt()
+    private val px48dp = TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 48f, context.resources.displayMetrics).toInt()
+    private val windowManager = context.getSystemService(WindowManager::class.java)
     private val bubbles = HashMap<Call, Bubble>()
     override fun onCallListChanged() {
         val liveCalls = CallList.getAllCalls()
@@ -25,13 +30,27 @@ class BubbleList(val context: Context) : CallList.Listener {
         }
 
         if (Settings.canDrawOverlays(context)) {
-            liveCalls.forEach {
-                if (!it.hasParent && !bubbles.containsKey(it)) {
-                    bubbles[it] = Bubble(context, it)
-                    bubbles[it]!!.show()
+            val windowSize = Point().apply { windowManager.defaultDisplay.getSize(this) }
+            liveCalls.forEach { call ->
+                if (!call.hasParent && !bubbles.containsKey(call)) {
+                    bubbles[call] = Bubble(context, call).apply {
+                        var defaultY = px64dp
+                        while (isSpaceUsed(defaultY)) {
+                            defaultY += px64dp
+                        }
+                        y = Math.min(defaultY, windowSize.y - px64dp)
+                        show()
+                    }
                 }
             }
         }
+    }
+
+    private fun isSpaceUsed(newY: Int): Boolean {
+        val windowSize = Point().apply { windowManager.defaultDisplay.getSize(this) }
+        return bubbles.map { it.value }
+                .filter { it.x < windowSize.x / 2 } // Bubble at left
+                .any { it.y in newY..(newY + px48dp) || newY in it.y..(it.y + px48dp) }
     }
 
     fun setUp() {
