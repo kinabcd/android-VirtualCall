86e61d70db3b30037322b3c7f9675019825d62eb
Kin Lo <kinabcd@gmail.com>
2022-07-20 10:52:35 +0800

    upgrade androidx

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -7,9 +7,9 @@ val sdf = SimpleDateFormat("yyyyMMdd")
 val kotlinVersion = getKotlinPluginVersion()
 val coreVersion = "1.8.0"
 val composeVersion = "1.1.1"
-val navVersion = "2.4.2"
-val lifecycleVersion = "2.4.1"
-val activityVersion = "1.4.0"
+val navVersion = "2.5.0"
+val lifecycleVersion = "2.5.0"
+val activityVersion = "1.5.0"
 
 plugins {
     id("com.android.application")
@@ -53,15 +53,23 @@ android {
 
 dependencies {
     implementation("org.jetbrains.kotlin:kotlin-stdlib:$kotlinVersion")
+    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8:$kotlinVersion")
+    implementation("org.jetbrains.kotlin:kotlin-reflect:$kotlinVersion")
+
     implementation("androidx.core:core-ktx:$coreVersion")
+    implementation("androidx.navigation:navigation-compose:$navVersion")
+
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:$lifecycleVersion")
+    implementation("androidx.lifecycle:lifecycle-viewmodel-compose:$lifecycleVersion")
+
+    implementation("androidx.activity:activity-ktx:$activityVersion")
+    implementation("androidx.activity:activity-compose:$activityVersion")
+
     implementation("androidx.compose.ui:ui:$composeVersion")
     implementation("androidx.compose.material:material:$composeVersion")
     implementation("androidx.compose.ui:ui-tooling-preview:$composeVersion")
     implementation("androidx.compose.foundation:foundation:$composeVersion")
     implementation("androidx.compose.foundation:foundation-layout:$composeVersion")
-    implementation("androidx.navigation:navigation-compose:$navVersion")
-    implementation("androidx.lifecycle:lifecycle-runtime-ktx:$lifecycleVersion")
-    implementation("androidx.activity:activity-ktx:$activityVersion")
     androidTestImplementation("androidx.compose.ui:ui-test-junit4:$composeVersion")
     debugImplementation("androidx.compose.ui:ui-tooling:$composeVersion")
 }
